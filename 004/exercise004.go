e01e4abb7c6779367070c83a41a6ec9677dc97d5
Toan Pham <pdtoanstock@gmail.com>
2022-08-22 21:53:39 +0700

    Using bufio to receive user's string input e.i 23, 45, 67

--- 004/exercise004.go
+++ 004/exercise004.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
+
+func main() {
+	var strInput string
+	fmt.Println("Enter a string ")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if scanner.Scan() { // wait for user's input from stdin ending up new line/ ENTER
+		strInput = scanner.Text()
+	}
+
+	fmt.Println(strInput)
+
+	result := Ex004(strInput)
+	fmt.Printf("%T, %v\n", result, result)
+
+}
 
 // Ex004 takes a string of comma-seperated numbers and returns a slice of int
 func Ex004(input string) []int {
 	// create a map with the size of n
 	numbers := strings.Split(input, ",")
 
+	// indentify length of Slide
 	length := len(numbers)
 	var num = make([]int, length)
 
