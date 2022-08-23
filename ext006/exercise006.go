37da598040d96b3c565a972476923251e3a864dc
Toan Pham <pdtoanstock@gmail.com>
2022-08-23 10:24:42 +0700

    Excercise 006

---
+++ ext006/exercise006.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Excercise 006")
+	fmt.Printf("Input list of numbers: ")
+	var input string
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input = scanner.Text()
+	}
+
+	numbers := Exe006(input)
+
+	fmt.Printf("Output: %T, %v\n", numbers, strings.Join(numbers, ","))
+
+}
+
+func Exe006(input string) []string {
+	const C, H = 50, 30
+	numStrings := strings.Split(input, ",")
+
+	length := len(numStrings)
+	var numbers = make([]string, length)
+
+	for index, v := range numStrings {
+		D, _ := strconv.Atoi(strings.Trim(v, " "))
+		calculatedVal := (2 * C * D) / H
+		numbers[index] = strconv.Itoa(calculatedVal)
+	}
+
+	return numbers
+}
